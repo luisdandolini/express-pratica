2b922669b87968cc3f70819102d9960d0b4906bb
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-05-12 16:27:01 -0300

    criando o servidor

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
+
+// Criando um servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+});
