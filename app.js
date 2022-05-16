a73171dc9b1a109702a7207c29bef2375dd2e3a6
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-05-16 16:03:03 -0300

    padronizando a nomenclatura das pastas

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const rotasProdutos = require('./Routers/rotasProdutos');
+const rotasProdutos = require('./routers/rotasProdutos');
 const app = express();
 
 const port = 3000;
